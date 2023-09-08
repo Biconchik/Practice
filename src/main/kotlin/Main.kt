d3214bb535dacbba7e341ed0da57d59c27d213b1
Bicochik <trockovaviktoria@gmail.com>
2023-09-08 12:21:50 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,72 @@
+fun main(args: Array<String>) {
+    val exercises: Int = 13
+    var exercisesSolved: Int = 0
+    exercisesSolved ++
+    println(exercisesSolved)
+    //создание константы "упражнения" и переменной "решено упражнений", решение 1-го упр
+
+    val myAge: Int = 18
+    exercisesSolved ++
+    println(myAge)
+    //создание константы "мой возраст" , решение 2-го упр
+
+    var averageOfAges: Double = 18.0
+    averageOfAges = (averageOfAges + 30) / 2
+    exercisesSolved ++
+    println(averageOfAges)
+    //создание переменной "среднее от возрастов", решение 3-го упр
+
+    val testNumber: Int = 3
+    val evenOdd: Int = testNumber % 2
+    exercisesSolved ++
+    println(evenOdd)
+    //создание переменной "", решение 4-го упр
+
+    var answer: Int = 0
+    answer = (((answer + 1) + 10) * 10 ) shr 3
+    exercisesSolved ++
+    println(answer)
+    //создание переменной "", решение 5-го упр
+
+    var age: Int
+    age = 16
+    println(age)
+    age = 30
+    println(age)
+    exercisesSolved ++
+    //создание переменной "", решение 6-го упр
+
+    val a: Int = 46
+    val b: Int = 10
+    val answer1: Int = (a * 100)+(b / 10)
+    println(answer1)
+    exercisesSolved ++
+    // варианты решений - 4610, 5600, 4601; решение 7-го упр
+
+    println((5 * 3) - (4 / 2) * 2)
+    exercisesSolved ++
+    //  решение - 11, решение 8-го упр
+
+    val a1: Double = 23.0
+    val b1: Double = 86.0
+    val average: Double = (a1 + b1) / 2
+    println(average)
+    exercisesSolved ++
+    // решение 9-го упр
+
+    val fahrenhei: Double = 64.0
+    val celcius: Double = (fahrenhei - 32) / 1.8
+    println(celcius)
+    exercisesSolved ++
+    // решение 10-го упр
+
+    val position: Int = 37
+    val rowrow: Int = position
+    val column: Int = position
+    println()
+    println()
+    // решение 11-го упр
+
+    
+}
+
